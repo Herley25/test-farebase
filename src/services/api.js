36594c76eff23eb780c8ca13edf0d7a5e133858b
Herley Oliveira <herleyoliveira19@gmail.com>
2021-09-23 00:41:00 -0300

    chore: add comment

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
-import axios from "axios";
+// import axios from "axios";
 
 
-const api = axios.create({
-    baseURL: 'https://sujeitoprogramador.com/'
-});
+// const api = axios.create({
+//     baseURL: 'https://sujeitoprogramador.com/'
+// });
 
-export default api;
+// export default api;
